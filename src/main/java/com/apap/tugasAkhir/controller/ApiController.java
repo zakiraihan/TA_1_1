01a78d65f4d1f340255a04c384bee2fac0d850f0
deana <deanalmira@gmail.com>
2018-11-23 20:16:46 +0700

    Deana: Fitur 3: Web Service untuk Pendaftaran Rawat Inap dari Sistem Informasi IGD

--- src/main/java/com/apap/tugasAkhir/controller/ApiController.java
+++ src/main/java/com/apap/tugasAkhir/controller/ApiController.java
@@ -1,5 +1,31 @@
 package com.apap.tugasAkhir.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apap.tugasAkhir.model.RequestPasienModel;
+import com.apap.tugasAkhir.service.RequestPasienService;
+import com.fasterxml.jackson.databind.JsonNode;
+
+@RestController
+@RequestMapping("/api")
 public class ApiController {
+	@Autowired
+	private RequestPasienService requestPasienService;
+	
+	@PostMapping(value = "/daftar-ranap")
+	private String addRequestPasien(@RequestBody JsonNode req) {
+		String resultString = req.get("idPasien").toString();
+		System.out.println(resultString);
+		RequestPasienModel newReq = new RequestPasienModel();
+		newReq.setAssign(0);
+		newReq.setIdPasien(Long.parseLong(resultString));
+		requestPasienService.addRequestPasien(newReq);
+		String returnMessage = "{\"status\" : 200, \"message\" : \"success\"}";
+		return returnMessage;
+	}
 
 }
